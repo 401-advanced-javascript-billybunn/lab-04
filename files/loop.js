4b6a115a6880a6ae15c90470342c1a3e30defecc
Billy Bunn <williammbunn@gmail.com>
2019-03-21 16:59:38 -0700

    creates loop.js

---
+++ files/loop.js
@@ -0,0 +1,10 @@
+/*
+- Using only buffers and bit manipulation, generate a file in the files folder called loop.js that initiates an array with 3 people's names, iterates them with a forEach loop, and console.log's each value.
+
+- You may not use Buffer.from() to simply create a buffer from chunks of your target code. Rather, you will need to work character by character.
+
+- Verify your work by running node loop.js from within the files folder and seeing the array items printed out.
+*/
+
+'use strict';
+
