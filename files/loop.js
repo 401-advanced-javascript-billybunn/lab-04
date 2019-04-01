59823dc01d83a1f62d1032403642e0ea008441cc
Billy Bunn <williammbunn@gmail.com>
2019-04-01 09:24:52 -0700

    starts refactor

---
+++ files/loop.js
@@ -0,0 +1,6 @@
+'use strict';
+
+(() => {
+  let names = ['Billy', 'Jon', 'Cory'];
+  names.forEach(name => console.log(name));
+})();
--- index.js
+++ index.js
@@ -53,30 +53,73 @@ function createArticle() {
 
 
 function insertHTMLTags(buffer) {
+  // Creates buffers for HTML tags
+  let articleTags = [Buffer.from('<article>'), Buffer.from('</article>')];
+  let h2Tags = [Buffer.from('<h2>'), Buffer.from('</h2>')];
+  let ulTags = [Buffer.from('<ul>'), Buffer.from('</ul>')];
+  let liTags = [Buffer.from('<li>'), Buffer.from('</li>')];
+  let h3Tags = [Buffer.from('<h3>'), Buffer.from('</h3>')];
+
 
   // Adds article tags to the beginning and end of my buffer
-  let articleTags = [Buffer.from('<article>'), Buffer.from('</article>')];
   let result = Buffer.concat([articleTags[0], buffer, articleTags[1]]);
 
-  // Wrap each section title in an h2 tag
-  let h2Tags = [Buffer.from('<h2>'), Buffer.from('</h2>')];
-  let firsth2Index = result.indexOf(`How does pair programming work?`); //?
-  let secondh2Index = result.indexOf(`
-While`); //?
-  let firstPart = result.slice(0, firsth2Index); //?
-  let partToh2Wrap = result.slice(firsth2Index, secondh2Index); //?
-  let secondPart = result.slice(secondh2Index, result.length); //?
+  // Find places to add h2 tags
+  for (let i = 0; i < result.length; i++) {
+    let beginning = Buffer.from(`
 
-  partToh2Wrap = Buffer.concat([h2Tags[0], partToh2Wrap, h2Tags[1]]); //?
-  result = Buffer.concat([firstPart, partToh2Wrap, secondPart]); //?
+`);
+    let end = Buffer.from(`
+`);
+    if (result[i] === beginning) {
+      let firstIndex = Buffer.indexOf(result[i]);
+      let secondIndex;
+      for (let i = 0; i < result.length; i++) {
+        if (result[i] === end) {
+          secondIndex = Buffer.indexOf(result[i]);
+          break;
+        }
+      }
+      let firstPart = result.slice(0, firstIndex);
+      let partToh2Wrap = result.slice(firstIndex, secondIndex);
+      let secondPart = result.slice(secondIndex, result.length);
+      partToh2Wrap = Buffer.concat([h2Tags[0], partToh2Wrap, h2Tags[1]]);
+      result = Buffer.concat([firstPart, partToh2Wrap, secondPart]);
+    }
+
+  }
+
+  // Wrap each section title in an h2 tag
+  //   let firsth2Index = result.indexOf(`How does pair programming work?`); //?
+  //   let secondh2Index = result.indexOf(`
+  // While`); //?
+  //   let firstPart = result.slice(0, firsth2Index); //?
+  //   let partToh2Wrap = result.slice(firsth2Index, secondh2Index); //?
+  //   let secondPart = result.slice(secondh2Index, result.length); //?
+
+  //   partToh2Wrap = Buffer.concat([h2Tags[0], partToh2Wrap, h2Tags[1]]); //?
+  //   result = Buffer.concat([firstPart, partToh2Wrap, secondPart]); //?
 
   return result;
 }
 
-// let testBuffer = Buffer.from(`ls.
+let testBuffer = Buffer.from(`ls.
 
-// How does pair programming work?
-// While`); //?
+How does pair programming work?
+While`);
+let beginning = Buffer.from(`
+
+`);
+console.log(beginning);
+for (let i = 0; i < testBuffer.length; i++) {
+  // beginning = Buffer.from(`ls`);
+  if (testBuffer[i] === beginning) {
+    // console.log(beginning.toString());
+    console.log('found it');
+  }
+  console.log(testBuffer[i]);
+}
+
 // testBuffer.length; //?
 // console.log(testBuffer); //?
 // let resultsBuf = insertHTMLTags(testBuffer); //?
